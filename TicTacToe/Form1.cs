606ad64bd8040c8489ece8b3cd01e2a8b938998b
me-adon <95122572+me-adon@users.noreply.github.com>
2022-03-21 22:00:15 +0600

    Add files via upload

---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tictactoe
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true;
+        int turn_count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else 
+                b.Text = "O";
+
+            turn = !turn;
+            b.Enabled = false;
+
+            turn_count++;
+
+            CheckForWinner();
+
+        }
+
+        private void CheckForWinner()
+        {
+            bool there_is_a_winner = false;
+
+            if((button1.Text == button2.Text) && (button2.Text == button3.Text) && (!button1.Enabled))
+            {
+                there_is_a_winner=true;
+            }
+            else if ((button4.Text == button5.Text) && (button5.Text == button6.Text) && (!button4.Enabled))
+            {
+                there_is_a_winner = true;
+            }
+            else if ((button7.Text == button8.Text) && (button8.Text == button9.Text) && (!button7.Enabled))
+            {
+                there_is_a_winner = true;
+            }
+
+
+            else if ((button1.Text == button4.Text) && (button4.Text == button7.Text) && (!button1.Enabled))
+            {
+                there_is_a_winner = true;
+            }
+            else if ((button2.Text == button5.Text) && (button5.Text == button8.Text) && (!button2.Enabled))
+            {
+                there_is_a_winner = true;
+            }
+            else if ((button3.Text == button6.Text) && (button6.Text == button9.Text) && (!button3.Enabled))
+            {
+                there_is_a_winner = true;
+            }
+
+
+            else if ((button1.Text == button5.Text) && (button5.Text == button9.Text) && (!button1.Enabled))
+            {
+                there_is_a_winner = true;
+            }
+            else if ((button3.Text == button5.Text) && (button5.Text == button7.Text) && (!button7.Enabled))
+            {
+                there_is_a_winner = true;
+            }
+            
+
+
+
+            if (there_is_a_winner)
+            {
+                disableButtons();
+
+                string winner = "";
+                if (turn)
+                    winner = "O";
+                else
+                    winner = "X";
+
+                MessageBox.Show(winner + " wins!");
+            }
+
+            else
+            {
+                if(turn_count == 9)
+                {
+                    MessageBox.Show("Match Draw");
+                }
+
+            }
+
+        }
+
+        private void disableButtons()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+            }
+
+            catch { }
+            
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turn_count = 0;
+
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }
+            }
+
+            catch { }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
