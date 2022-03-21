f878b0a20613c42e6d20be24bce9b81571d8e4eb
me-adon <95122572+me-adon@users.noreply.github.com>
2022-03-21 23:17:38 +0600

    Add files via upload

---
+++ Calculator_Form/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonToAdd_Click(object sender, EventArgs e)
+        {
+            int N1=Convert.ToInt32(textBox1.Text); //converts input in 1st text box to integer
+            int N2=Convert.ToInt32(textBox2.Text);   
+            string result=Convert.ToString(N1+N2); //converts the evaluated value to string to give output
+            MessageBox.Show(result);
+            //Convert.ToString(textBox3) = result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int N1 = Convert.ToInt32(textBox1.Text); //converts input in 1st text box to integer
+            int N2 = Convert.ToInt32(textBox2.Text);
+            string result = Convert.ToString(N1 - N2); //converts the evaluated value to string to give output
+            MessageBox.Show(result);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int N1 = Convert.ToInt32(textBox1.Text); //converts input in 1st text box to integer
+            int N2 = Convert.ToInt32(textBox2.Text);
+            string result = Convert.ToString(N1 * N2); //converts the evaluated value to string to give output
+            MessageBox.Show(result);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int N1 = Convert.ToInt32(textBox1.Text); //converts input in 1st text box to integer
+            int N2 = Convert.ToInt32(textBox2.Text);
+            string result = Convert.ToString(N1 / N2); //converts the evaluated value to string to give output
+            MessageBox.Show(result);
+        }
+    }
+}
