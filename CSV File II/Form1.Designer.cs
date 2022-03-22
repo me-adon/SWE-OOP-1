3ffb3f2e13fecc6d30de270f5e2b7b1313002bca
me-adon <95122572+me-adon@users.noreply.github.com>
2022-03-22 23:54:12 +0600

    Add files via upload

---
+++ CSV File II/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace CSV_File_II
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(54, 132);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 355);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(180, 132);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 355);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(306, 132);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 355);
+            this.listBox3.TabIndex = 2;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(432, 132);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(120, 355);
+            this.listBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(790, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(830, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(757, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Attendance :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(790, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Quiz I :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(790, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Quiz II :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(774, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Quiz III :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(772, 323);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Quiz IV :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(747, 368);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Best 3 Quizes :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(790, 404);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Mid :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(790, 443);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Final :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(830, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SearchOnClick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(839, 154);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(839, 187);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(839, 232);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(839, 277);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 18;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(839, 320);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 19;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(839, 365);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 20;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(839, 401);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 21;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(839, 440);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(785, 484);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(34, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Viva :";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(839, 481);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 24;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(750, 519);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(69, 13);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Total Marks :";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(839, 516);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 26;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(751, 554);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(68, 13);
+            this.label12.TabIndex = 27;
+            this.label12.Text = "Percentage :";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(839, 551);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 20);
+            this.textBox12.TabIndex = 28;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(772, 590);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(42, 13);
+            this.label13.TabIndex = 29;
+            this.label13.Text = "Grade :";
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(839, 587);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(100, 20);
+            this.textBox13.TabIndex = 30;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 624);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox13;
+    }
+}
+
---
+++ CSV File II/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace CSV_File_II
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         public List<Student> students = new List<Student>();
+         public List<int> quizes = new List<int>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var reader = new StreamReader(@"C:\Users\Adon\Desktop\SWE4201MarkSheet.csv"))
+            {
+                //List<string> listA = new List<string>();
+                //List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    if (values[2] == "")
+                    {
+                        values[2] = "0";
+                    }
+                    if (values[3] == "")
+                    {
+                        values[3] = "0";
+                    }
+                    if (values[4] == "")
+                    {
+                        values[4] = "0";
+                    }
+                    if (values[5] == "")
+                    {
+                        values[5] = "0";
+                    }
+                    if (values[6] == "")
+                    {
+                        values[6] = "0";
+                    }
+                    if (values[8] == "")
+                    {
+                        values[8] = "0";
+                    }
+                    if (values[9] == "")
+                    {
+                        values[9] = "0";
+                    }
+
+                    Student student = new Student();
+                    student.ID = values[0];
+                    student.Name = values[1];
+                    student.Attendance = Convert.ToInt32(values[2]);
+                    student.QuizI = Convert.ToInt32(values[3]);
+                    student.QuizII = Convert.ToInt32(values[4]);
+                    student.QuizIII = Convert.ToInt32(values[5]);
+                    student.QuizIV = Convert.ToInt32(values[6]);
+                    student.MID = Convert.ToInt32(values[7]);
+                    student.Final= Convert.ToInt32(values[8]);
+                    student.Viva = Convert.ToInt32(values[9]);
+
+
+                    int q1, q2, q3, q4,temp;
+                    q1 = student.QuizI;
+                    q2 = student.QuizII;
+                    q3 = student.QuizIII;
+                    q4 = student.QuizIV;
+
+                    if(q1>q4 && q2>q4 && q3>q4)
+                    {
+                        student.bestQuizIII = q1 + q2 + q3;
+                    }
+                    if (q1 > q3 && q2 > q3 && q4 > q3)
+                    {
+                        student.bestQuizIII = q1 + q2 + q4;
+                    }
+                    if (q1 > q2 && q3 > q2 && q4 > q2)
+                    {
+                        student.bestQuizIII = q1 + q4 + q3;
+                    }
+                    if (q1 < q4 && q2 > q1 && q3 > q1)
+                    {
+                        student.bestQuizIII = q4 + q2 + q3;
+                    }
+
+
+                    /*
+                    quizes.Add(student.QuizI);
+                    quizes.Add(student.QuizII);
+                    quizes.Add(student.QuizIII);
+                    quizes.Add(student.QuizIV);
+
+                    quizes.Sort();*/
+
+                    //student.bestQuizIII = quizes[1] + quizes[2] + quizes[3];
+                    int sum = student.bestQuizIII + student.MID + student.Final + student.Viva;
+                    student.totalMarks = sum;
+                    student.Percentage = student.totalMarks / 3;
+ 
+                    
+
+
+                    if (student.Percentage >= 80)
+                    {
+                        student.grade = "A+";
+                    }
+                    else if (student.Percentage < 80 && student.Percentage >= 75)
+                    {
+                        student.grade = "A";
+                    }
+                    else if (student.Percentage < 75 && student.Percentage >= 70)
+                    {
+                        student.grade = "A-";
+                    }
+                    else if (student.Percentage < 70 && student.Percentage >= 65)
+                    {
+                        student.grade = "B+";
+                    }
+                    else if (student.Percentage < 65 && student.Percentage >= 60)
+                    {
+                        student.grade = "B";
+                    }
+                    else if (student.Percentage < 60 && student.Percentage >= 55)
+                    {
+                        student.grade = "C";
+                    }
+                    else if (student.Percentage < 55 && student.Percentage >= 50)
+                    {
+                        student.grade = "D";
+                    }
+                    else
+                    {
+                        student.grade = "F";
+                    }
+
+                    students.Add(student);
+
+                    listBox1.Items.Add(student.ID);
+                    listBox2.Items.Add(student.Name);
+                    listBox3.Items.Add(student.Percentage + "%");
+                    listBox4.Items.Add(student.grade);
+
+                    //listBox3.Items.Add(student.Attendance);
+
+                    //listA.Add(values[0]);
+                    //listB.Add(values[1]);
+                }
+            }
+        }
+
+        private void SearchOnClick(object sender, EventArgs e)
+        {
+            string id = textBox1.Text;
+
+            for(int i = 0; i < students.Count; i++)
+            {
+                if(students[i].ID == id)
+                {
+                    textBox2.Text = Convert.ToString(students[i].Attendance);
+                    textBox3.Text = Convert.ToString(students[i].QuizI);
+                    textBox4.Text = Convert.ToString(students[i].QuizII);
+                    textBox5.Text = Convert.ToString(students[i].QuizIII);
+                    textBox6.Text = Convert.ToString(students[i].QuizIV);
+                    textBox7.Text = Convert.ToString(students[i].bestQuizIII);
+                    textBox8.Text = Convert.ToString(students[i].MID);
+                    textBox9.Text = Convert.ToString(students[i].Final);
+                    textBox10.Text = Convert.ToString(students[i].Viva);
+                    textBox11.Text = Convert.ToString(students[i].totalMarks);
+                    textBox12.Text = Convert.ToString(students[i].Percentage);
+                    textBox13.Text = Convert.ToString(students[i].grade);
+                }
+            }
+        }
+    }
+}
